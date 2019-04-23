cd8a149c628e3e1569bdbe5918b06ab05354dee4
xb12369 <2940366047@qq.com>
2019-04-23 22:37:41 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/beiyeixiangmu/beiyeixiangmu/BeiyeixiangmuApplication.java
@@ -0,0 +1,13 @@
+package beiyeixiangmu.beiyeixiangmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiyeixiangmuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeiyeixiangmuApplication.class, args);
+    }
+
+}
